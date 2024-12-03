ae66a7fba2eeb11cfd5f2a5e4e7a19e06991de49
goutham120203 <gouthampaladugu12@gmail.com>
2024-12-03 23:32:57 +0530

    onsubmit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import React,{useState} from 'react';
 import './App.css';
 
 const App = () => {
-const [user,setUser] = useState("");
-const handler = e =>{
-  setUser(e.target.value)
+const [data,setData] = useState({
+  username:'',
+  password:''
+});
+const {username,password} = data;
+const onChange = e =>{
+  setData({...data,[e.target.name]:[e.target.value]})
+}
+const submitHandler = e =>{
+  e.preventDefault();
+  console.log(data);
 }
 
   return (
     <div>
       <center>
-        <input type='text' placeholder='username' value={user} name='user' onChange={handler}/> <br />
-        {user}
+      <form onSubmit={submitHandler}>
+        <input type='text' name='username' value={username}  
+        onChange={onChange} placeholder='Username'/><br />
+
+        <input type='password' name='password' value={password}
+        onChange={onChange} placeholder='password' /><br />
+
+        <input type='submit' name='submit' />
+        </form>
       </center>
     </div>
   )
