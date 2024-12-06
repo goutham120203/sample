0a6b320ddef67bb566c780c2821fe7ca5b07c149
goutham120203 <gouthampaladugu12@gmail.com>
2024-12-06 21:47:19 +0530

    using the filter

--- src/App.js
+++ src/App.js
@@ -3,34 +3,15 @@ import React from 'react';
 import './App.css'
 
 const App = () => {
-  const arr = [
-  {
-    id:1,
-    title:"java"
-  }, 
-  {
-    id:2,
-    title:"react"
-  }, 
-  {id:3,
-    title:"springboot"
-  }, 
-  {
-    id:4,
-    title:"mongo"
-  }];
+  const names = ["cat","cool","dog"];
+  const filtered = names.filter(name => name.includes('c'))
   return (
     <div>
-      <ul>
-        {
-          arr.map(
-            (value,index) => <li key={value.id}>{value.title}</li>
-          )
-        }
-      </ul>
+      {
+        filtered.map(item => <li>{item}</li>)
+      }
     </div>
   );
 };
 
 export default App;
-
