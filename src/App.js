28edbde0f79d6f0dd7a8bda7e2b7dfdbbe4ec149
goutham120203 <gouthampaladugu12@gmail.com>
2024-12-07 14:13:10 +0530

    basic sinup form

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 const App = () => {
   const [data, setData] = useState({
     username: '',
-    password: ''
+    email:'',
+    password: '',
+    confirmpassword:''
   });
-  const { username, password } = data;
+  const { username, email, password, confirmpassword } = data;
   const changeHandler = e => {
     setData({...data,[e.target.name]:e.target.value})
   }
   const submitHandler = e =>{
     e.preventDefault()
-    console.log(data)
+    if(password === confirmpassword){
+    console.log(data);
+    }
+    else{
+      console.log("confirmpassword is not matching");
+    }
   }
   return (
     <div>
       <center>
         <form onSubmit={submitHandler}>
           <input type='text' name='username' value={username} placeholder="Username" onChange={changeHandler} /> <br />
+          <input type='text' name='email' value={email} placeholder="Email" onChange={changeHandler} /> <br />
           <input type='password' name='password' value={password} placeholder="Password" onChange={changeHandler} /> <br />
+          <input type='password' name='confirmpassword' value={confirmpassword} placeholder="confirmpassword" onChange={changeHandler} /> <br />
           <input type='submit' name='submit' />
         </form>
       </center>
