cba22389ef85d2988c3d22e6d32a965006631e00
goutham120203 <gouthampaladugu12@gmail.com>
2024-12-11 23:32:54 +0530

    axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect,useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
   const [data,setData] = useState([])
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos').then(
-      response => response.json() 
-    ).then(json => setData(json))
-  },
-  )
+    axios.get('https://jsonplaceholder.typicode.com/todos').then(
+      reponse => setData(reponse.data)
+    )
+  },[])
   return (
     <div>
       {data.map(item => <li key={item.id}>{item.title}</li> )}
