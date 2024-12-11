ade9a8e19f82aab2417944ad35df99652e390421
goutham120203 <gouthampaladugu12@gmail.com>
2024-12-11 22:54:56 +0530

    fetch the data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [input,setInput] = useState("");
-  const [result,setResult] = useState(0);
-  const handler = e =>{
-    setInput(e.target.value);
-  }
+  const [data,setData] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos').then(
+      response => response.json() 
+    ).then(json => setData(json))
+  },
+  )
   return (
     <div>
-      <center>
-        <input type='text' name="input" value={input} onChange={handler}/>
-        <br />
-        <button onClick={()=>setResult(eval(input))}>Result</button>
-        <h4>Result is : {result}</h4>
-
-        <button onClick={()=>setInput(input+'1')}>1</button>
-        <button onClick={()=>setInput(input+'2')}>2</button>
-        <button onClick={()=>setInput(input+'3')}>3</button> 
-        <button onClick={()=>setInput(input+'4')}>4</button>
-        <button onClick={()=>setInput(input+'5')}>5</button> <br />
-        <button onClick={()=>setInput(input+'6')}>6</button> 
-        <button onClick={()=>setInput(input+'7')}>7</button>
-        <button onClick={()=>setInput(input+'8')}>8</button>
-        <button onClick={()=>setInput(input+'1')}>9</button>
-        <button onClick={()=>setInput(input+'0')}>0</button> <br />
-
-        <button onClick={()=>setInput(input+'+')}>+</button>
-        <button onClick={()=>setInput(input+'-')}>-</button>
-        <button onClick={()=>setInput(input+'*')}>*</button>
-        <button onClick={()=>setInput(input+'/')}>/</button>
-        <button onClick={()=>setInput('')}>clr</button> <br />
-      </center>
+      {data.map(item => <li key={item.id}>{item.title}</li> )}
     </div>
   );
 };
