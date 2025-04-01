6cc02306e065c9a01b49a3617ac69f481cc57ff4
goutham120203 <gouthampaladugu12@gmail.com>
2025-04-01 11:36:54 +0530

    Revert "deleted the componets"
    This reverts commit a2d43a3a2916d68390bf3df314b298cf279e800d.

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    const styles = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // Full viewport height
+        textAlign: "center",
+      };
+    return (
+        <div style={styles}>
+                <h1>about page</h1>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Dashboard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Dashboard = () => {
+    const styles = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // Full viewport height
+        textAlign: "center",
+      };
+    return (
+        <div style={styles}>
+
+                <h1>dashboard page</h1>
+
+        </div>
+    )
+}
+
+export default Dashboard
