790aada4172493245ae3a3de5d9c25ab08fd7304
goutham120203 <gouthampaladugu12@gmail.com>
2025-04-01 23:28:53 +0530

    Add the count in cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
-import React from "react";
+import React,{ useState } from 'react';
+
+let stock = 10;
 
 function App(){
+  let [count, setCount] = useState(0);
 return(
   <div className="Wrapper">
-    <h1>Change the backgroundColor</h1>
-    <input type="color" onChange={(e)=>{document.body.style.backgroundColor=e.target.value}}/><br></br>
+    <button disabled={count === 0}onClick={()=>{
+      if(count>0){
+            setCount(count -1)
+      }
+    }}>-</button>
+    <p>{count}</p>
+    <button disabled={count===10} onClick={()=>{
+      if(count<stock){
+        setCount(count+1)
+      }
+    }}>+</button>
     
   </div>
 )
