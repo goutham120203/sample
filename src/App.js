2eab4f24d5823297c1efaf3362b328c6d4cac602
goutham120203 <gouthampaladugu12@gmail.com>
2025-04-04 00:30:12 +0530

    QueryParams

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, useParams,Routes,Route } from "react-router-dom";
 
-const Home = () =><h1>Home Page</h1>
-const About = () =><h1>About Page</h1>
-
+const UserProfile = ()=>{
+  const {id} = useParams();
+  return <h1>user Profile:{id}</h1>
+}
 
 function App(){
-  return(
-    <BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-        
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    return(
+      <BrowserRouter>
+        <nav>
+          <Link to="/user/1" >User 1</Link> | <Link to="/user/2">User 2</Link>
+        </nav>
+          <Routes>
+            <Route path="/user/:id" element={<UserProfile/>}/>
+          </Routes>
+      </BrowserRouter>
+    )
 }
 
 export default App
